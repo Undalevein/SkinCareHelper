a3ae686735704a252fbf5bbc861fd269d0264d84
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2023-04-16 16:46:35 -0700

    Adding rudimentary stuff to my branch. Nothign special.

---
+++ CareTTNode.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class CareTTNode{
+    boolean wordEnd;
+    char letter;
+    TTNode left, mid, right;
+            
+    /**
+     * Constructs a new TTNode containing the given character
+     * and whether or not it represents a word-end, which can
+     * then be added to the existing tree.
+     * @param letter Letter to store at this node
+     * @param wordEnd Whether or not this is a word-ending letter
+     */
+    TTNode (char letter, boolean wordEnd) {
+        this.letter  = letter;
+        this.wordEnd = wordEnd;
+                
+    }
+
+    public void insert(String str){
+        if (){
+            
+        }
+    }
+
+
+}
---
+++ Helper.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Helper{
+    public static void main(String[] args){
+        TTNode database = new TTNode(" ", false);
+
+
+
+
+        /**
+         * Internal storage of textfiller search terms
+         * as represented using a Ternary Tree (TT) with TTNodes
+         * [!] Note: these are currently implemented for the base-assignment;
+         *     those endeavoring the extra-credit may need to make changes
+         *     below (primarily to the fields and constructor)
+         */
+        private class TTNode {
+            
+            
+            
+        }
+
+
+
+
+    }
+}
